c998800a405131291c696f2126ebc0ec7561b7c3
Patricio Rodriguez <patriciorodriguez080@gmail.com>
2026-05-17 17:14:41 -0300

    Ajuste en condiciones

--- validaciones-miembros.c
+++ validaciones-miembros.c
@@ -12,23 +12,23 @@ int validarDNI(long dni){
 }
 
 int validarSexo(char sexo){
-    if (sexo != 'F' && sexo != 'M' && sexo != 'O'){
+    if (sexo != 'F' || sexo != 'M' || sexo != 'O'){
         return ERROR_SEXO;
     }
     return VALIDACION_OK;
 }
 
 int validarEstado(char estado){
-    if (estado != 'A' && estado != 'B'){
+    if (estado != 'A' || estado != 'B'){
         return ERROR_ESTADO;
     }
     return VALIDACION_OK;
 }
 
 int validarPlan(const char *plan){
-    if (strcmp(plan, "BASIC")   != 0 &&
-        strcmp(plan, "PREMIUM") != 0 &&
-        strcmp(plan, "VIP")     != 0 &&
+    if (strcmp(plan, "BASIC")   != 0 ||
+        strcmp(plan, "PREMIUM") != 0 ||
+        strcmp(plan, "VIP")     != 0 ||
         strcmp(plan, "FAMILY")  != 0){
             return ERROR_PLAN;
         }
