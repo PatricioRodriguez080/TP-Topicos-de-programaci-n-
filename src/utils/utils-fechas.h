7f60ca2c3a16da9289bd3af60ac2325dbb060b30
Patricio Rodriguez <patriciorodriguez080@gmail.com>
2026-05-20 01:25:29 -0300

    Funcion para pedir fecha de proceso inicial

--- src/utils/utils-fechas.h
+++ src/utils/utils-fechas.h
@@ -12,5 +12,6 @@ int esFechaValida(t_fecha fecha);
 int compararFechas(t_fecha fechaA, t_fecha fechaB);
 int calcularEdad(t_fecha fechaNacimiento, t_fecha fechaProceso);
 int parsearFecha(const char *str, t_fecha *fecha);
+t_fecha pedirFechaProceso(void);
 
 #endif
